4e3fd24005f9ea1acb65bfc9c2288c4c6716996b
nausicaa01 <allgatorboogaloo.mix@gmail.com>
2015-06-04 16:16:57 +0900

    6.2.5まで

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,65 @@
+# -*- encoding: utf-8 -*-
+require 'spec_helper'
+
+describe User do
+
+  before do
+    @user = User.new(name: "Example User", email: "user@example.com")
+  end
+
+  subject { @user }
+
+  #name属性のデータがある
+  it { should respond_to(:name) }
+  #email属性のデータがある
+  it { should respond_to(:email) }
+
+  #@userがvalidである
+  it { should be_valid }
+
+  describe ":nameが空の場合のテスト" do
+    before { @user.name = " " }
+    #validではないと検証する
+    it { should_not be_valid }
+  end
+
+  describe "when name is too long" do
+    before { @user.name = "a" * 51 }
+    it { should_not be_valid }
+  end
+
+  describe ":emailが空の場合のテスト" do
+    before { @user.email = " " }
+    #validではないと検証する
+    it { should_not be_valid }
+  end
+
+  describe "emailフォーマットのテスト" do
+    it "emailフォーマットが不正な場合" do
+      addresses = %w[user@foo,com user_at_foo.org example.user@foo.
+                     foo@bar_baz.com foo@bar+baz.com]
+      addresses.each do |invalid_address|
+        @user.email = invalid_address
+        expect(@user).not_to be_valid
+      end
+    end
+ 
+    it "emailフォーマットが正しい場合" do
+      addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
+      addresses.each do |valid_address|
+        @user.email = valid_address
+        expect(@user).to be_valid
+      end
+    end
+  end
+
+  describe "email addressが登録済みの場合" do
+    before do
+      user_with_same_email = @user.dup
+      user_with_same_email.email = @user.email.upcase
+      user_with_same_email.save
+    end
+    #validではないと検証する
+    it { should_not be_valid }
+  end
+end
