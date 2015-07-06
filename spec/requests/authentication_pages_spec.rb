b92d24a2000a29995a3a847aedc32a86d9cb51d3
nausicaa01 <allgatorboogaloo.mix@gmail.com>
2015-07-06 10:09:47 +0900

    9.3.1

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -29,15 +29,10 @@ describe "Authentication" do
 
     describe "with valid information" do
       let(:user) { FactoryGirl.create(:user) }
-      #before do
-      #  visit signin_path
-      #  fill_in "Email",    with: user.email.upcase
-      #  fill_in "Password", with: user.password
-      #  click_button "Sign in"
-      #end
       before { valid_signin user }
 
       it { should have_title(user.name) }
+      it { should have_link('Users', href: users_path) }
       it { should have_link('Profile', href: user_path(user)) }
       it { should have_link('Settings', href: edit_user_path(user)) }
       it { should have_link('Sign out', href: signout_path) }
@@ -81,6 +76,11 @@ describe "Authentication" do
         describe "submitting to the update action" do
           before { patch user_path(user) }
           specify { expect(response).to redirect_to(signin_path) }
+        end
+
+        describe "visiting the user index" do
+          before { visit users_path }
+          it { should have_title('Sign in') }
         end
       end
     end
